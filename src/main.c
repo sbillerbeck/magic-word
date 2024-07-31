25f96c6a8bbca10ad8e2bf46c802b8f03906e270
osadmin <osadmin@pearl.cs.nor.ou.edu>
2024-07-31 16:54:48 -0500

    Commented gif, cli only

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ int main()
     if(is_permission == false && attempt_number == MAX_ATTEMPT)
     {
       print_password_denied(attempt_number, MAX_ATTEMPT);
-      system("qlmanage -p ./data/ned.gif");
+      //system("animate ./data/ned.gif");
       animate();
     }
     else if(is_permission == true)
